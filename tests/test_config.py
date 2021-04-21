5565a55dddd8caf18288db57a10734fbfbd2faf9
unknown <affanaminn@gmail.com>
2021-04-21 15:55:23 +0500

    Web Structure Is Added

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,13 @@
 
 
+
+class NotinRange(Exception):
+    def __init__(self,message = "Value Not in Range"):
+        self.message=message
+        super().__init__(self.message)
+
 def test_generic():
     a=2
     b=2
     assert a == b
+
