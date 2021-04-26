ef02ca9d45d0fda369f359a1aea00f85cf01ff1f
unknown <affanaminn@gmail.com>
2021-04-26 16:01:11 +0500

    Testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import json
 @pytest.fixture
 def config(config_path="params.yaml"):
     with open(config_path) as yaml_file:
-        config = pyyaml.safe_load(yaml_file)
+        config = yaml.safe_load(yaml_file)
     return config
 
 @pytest.fixture
