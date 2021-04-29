da6355cd28a676067e55d28c7c68043a65c4eaf8
unknown <affanaminn@gmail.com>
2021-04-29 13:29:04 +0500

    Working On Deployment 007

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True)
+    #app.run(host='localhost', port=5000, debug=True)
     #app.run(host='0.0.0.0', port=5000, debug=True)
 
