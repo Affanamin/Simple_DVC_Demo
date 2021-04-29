80350932dda4a48d86781eabc6530071a3ac6b89
unknown <affanaminn@gmail.com>
2021-04-29 13:11:01 +0500

    Working On Deployment 002

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,7 @@ import json
 @pytest.fixture
 def config(config_path="params.yaml"):
     with open(config_path) as yaml_file:
-        #config = ""
         config = yaml.safe_load(yaml_file)
-
     return config
 
 @pytest.fixture
